a21c3b1edc82b1fb2b6ef41bb07687cfa96fdc7a
Bernard Almeida da Costa <biernard@gmail.com>
2023-12-11 15:29:54 +0100

    updates on Section 2 and 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from "react";
+import Section2 from "./Components/Section2/Section2";
+import Section3 from "./Components/Section3/Section3";
+import "./App.css";
 
 function App() {
- 
-
   return (
     <>
-     
+      <Section2 />
+      {/* <Section3 /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
