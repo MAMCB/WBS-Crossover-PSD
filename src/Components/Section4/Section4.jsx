d340e2c5f2f79c2df4524a03c5a874152afe388e
MAMCB <96302464+MAMCB@users.noreply.github.com>
2023-12-11 13:57:32 +0100

    finished section 4

--- src/Components/Section4/Section4.jsx
+++ src/Components/Section4/Section4.jsx
@@ -17,6 +17,7 @@ const Section4 = () => {
   return (
     <div className="section">
       <div className="subSection">
+        <h4 id="first">Fast & Easy</h4>
         <h1>Get Your Favourite Resort Bookings</h1>
 
         <div className="bulletPoint">
